5411d8d1beacdabe887e6a722332c1565c2bac21
Anil <an.kumar3407@gmail.com>
2024-12-19 12:22:44 +0530

    first Commit

---
+++ src/main/java/com/demo/TelephonicBacknedApplication.java
@@ -0,0 +1,13 @@
+package com.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TelephonicBacknedApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TelephonicBacknedApplication.class, args);
+	}
+
+}
